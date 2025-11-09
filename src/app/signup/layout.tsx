0fa8fad86d3dead52ecee7bb6f9e4c75b7b04b10
swaraj608 <singhaniajahnvi260@gmail.com>
2025-11-09 20:39:02 +0530

    Added layout.tsx to login/signup

---
+++ src/app/signup/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+
+export default function SignupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-[#0b0c10] text-white">
+      <Navbar />
+      <main className="pt-20">{children}</main>
+    </div>
+  );
+}
